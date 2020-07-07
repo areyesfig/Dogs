c5f4bc9d34120bafeb6b95e686ac5c9b3d39e7a4
areyesfig <areyes.088@gmail.com>
2020-07-06 23:12:59 -0400

    Initial commit

---
+++ app/src/main/java/com/devtides/dogs/view/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.devtides.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.devtides.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+import com.devtides.dogs.view.DetailFragmentArgs
+
+
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+   
+}
